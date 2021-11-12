6a14307fa5bd47f2f84c99bfc89708258723122b
KuBerlinON <bkuberlinov@gmail.com>
2021-11-12 10:00:58 +0100

    conditional rendering of the pages added

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  name: string = '';
+  page: string = "recipes";
+  onNavBtnClicked(page: string) {
+    this.page = page
+  }
 }
